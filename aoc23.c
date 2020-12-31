8492170ea62f071f4f93198851c4477e5bf188fd
Martin Boekhoff <martin.ongsiek@gmail.com>
2020-12-31 17:12:50 +0100

    part 2 day 23, rewrite with special linked list.

--- aoc23.c
+++ aoc23.c
@@ -5,104 +5,91 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-int take3(char *inOut, char *three, int position, int len) {
-    int result = len - 3;
-    for (int i = 0; i < 3; i++) { // copy to three
-        int x = (position + i + 1) % len;
-        three[i] = inOut[x];
-    }
-    int next = position + 4;
-    if (next > len) { // overlab. if == len no nothing to do.
-        next -= len;
-        for (int i = 0; i < next; i++) { // copy to three
-            inOut[i] = ' ';
-            result++;
-        }
-    } else {
-        for (int i = next; i < len; i++) { // copy to three
-            inOut[i - 3] = inOut[i];
-        }
-    }
-    inOut[result] = 0;  // for debugging
-    return result;
+struct linkedCircle {
+    int val;
+    struct linkedCircle *next;
+};
+
+typedef struct linkedCircle linkedCircle_t;
+
+int decFind(int find, int len) {
+    return --find <= 0 ? len - 1 : find;
 }
 
-int getPosition(char inOut[10], char find, int len) {
-    for (int i = 0; i < len; i++) { // copy to three
-        if (inOut[i] == find) {
-            return i;
-        }
-    }
-    return 0;
-}
-
-int getDestination(char inOut[10], int position, int len) {
-    char find = inOut[position] - 1;
-    while (1) {
-        if (find <= '0') {
-            find = '9';
-        }
-        for (int i = 0; i < len; i++) { // copy to three
-            if (inOut[i] == find) {
-                return i;
-            }
-        }
-        find--;
-    }
-}
-
-void insert3(char inOut[10], char three[4], int position,  int cur, int len) {
-    if (position <= cur) {  // don´t move current
-        // fillup end
-        int x = cur - len + 4; // fix spaces at begin
-        x = x > 0 ? x : 0;
-        int to = (len - 3 + x) % len;
-        for (int i = x; i < position; i++) {
-            inOut[to] = inOut[i];
-            to = (to + 1) % len;
-        }
-        //insert
+linkedCircle_t *findDest(linkedCircle_t *all, linkedCircle_t *cur, linkedCircle_t *three, int len) {
+    int find = decFind(cur->val, len);
+    linkedCircle_t *help;
+    for (int j = 0; j < 3; j++) {
+        help = three;
         for (int i = 0; i < 3; i++) {
-            inOut[to] = three[i];
-            to = (to + 1) % len;
-        }
-    } else {
-        for (int i = 0; i < 3; i++) {
-            int to = position + i;
-            if (to + 3 < len) {
-                inOut[to + 3] = inOut[to];
+            if (help->val == find) {
+                find = decFind(find, len); 
             }
-            inOut[to] = three[i];
+            help = help->next;
         }
     }
+    return &all[find]; 
 }
 
-char *crabCups(char inOut[10], int rounds) {
-    int cur = 0;
-    char three[4] = {0};
-    int len = strlen(inOut);
+uint64_t crabCups(char inOut[10], int rounds, bool part2) {
+    int slen = strlen(inOut);
+    int len = part2 ? 1000001 : slen + 1;
+    
+    linkedCircle_t *lc = (linkedCircle_t*) malloc(len * sizeof *lc);
+    linkedCircle_t *next, *cur, *three, *dest = 0;
+    // init linked list (ring)
+    for (int i = 0; i < slen; i++) {
+        int val = inOut[i] - '0';
+        dest = next;
+        next = &lc[val];
+        next->val = val;
+        if (i  == 0) {
+            cur = next;
+            cur->next = 0;
+
+        } else {
+            dest->next = next;
+        }
+    }
+    if (part2) {
+        for (int i = slen + 1; i < len; i++) {
+            dest = next;
+            next = &lc[i];
+            next->val = i;
+            dest->next = next;
+        }
+    }
+    next->next = cur; // close ring
+    // play the game
     for (int r = 0; r < rounds; r++) {
-        // puts(inOut);
-        int l = take3(inOut, three, cur, len);
-        int dest = getDestination(inOut, cur, l);
-        insert3(inOut, three, dest + 1, cur, len);
-        cur = (cur + 1) % len;
+        // take three elements
+        three = cur->next;
+        cur->next = three->next->next->next;
+        dest = findDest(lc, cur, three, len);
+        three->next->next->next = dest->next;
+        dest->next = three;    
+        cur = cur->next;
     }
-    // puts(inOut);
-    int pos = getPosition(inOut, '1', len);
-    char help[10] = {0};
-    memcpy(help, inOut, len);
-    int from = (pos + 1) % len;
-    for (int i = 0; i < len - 1; i++) {
-        inOut[i] = help[from];
-        from = (from + 1) % len;
+    if (part2) {
+        int ret = lc[1].next->val * lc[1].next->next->val;
+        free(lc);
+        return ret;
+    } else {
+        char buf[10];
+        next = lc[1].next;
+        for (int i = 0; i < len - 2; i++) {
+            buf[i] = '0' + next->val;
+            next = next->next;
+        }
+        buf[len - 1] = 0;
+        free(lc);
+        return strtol(buf, 0, 0);
     }
-    inOut[len - 1] = 0;
-    return inOut;
 }
 
 int main(void) {
-    char in[] = "487912365";
-    printf("Part 1: %s\n", crabCups(in, 100));
+    char in[] = "487912365"; // Test "389125467";
+    printf("Part 1: %llu\n", crabCups(in, 100, false));
+    printf("Part 2: %llu\n", crabCups(in, 10000000, true));
     return 0;
 }
