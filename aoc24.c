ca50051bd060d45acc0aadb09affe9edb67e1714
Martin Boekhoff <martin.ongsiek@gmail.com>
2021-01-02 00:32:46 +0100

    got day 24 working, but part 2 is not optimal

--- aoc24.c
+++ aoc24.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-/* hexagonal memory mapping
+/* hexagonal coordinate calculating
             +---x--->
             |   | 0 | 2 | 4 | 6          y=-1
 -4-2 0 2 4  y  /'\./'\./'\./'\./     "\
@@ -27,11 +27,13 @@ ne   1  0     1 -1
 typedef struct {
     int x;
     int y;
-    bool on;
+    bool black;
 } tile_t;
 
-tile_t tile[500];
-int tileCount = 0;
+typedef struct {
+    tile_t tile[4000];
+    int tileCount;
+} floor_t;
 
 tile_t getCoord(char *str) {
     tile_t t = { 0, 0, false };
@@ -60,38 +62,104 @@ tile_t getCoord(char *str) {
     return t;
 }
 
-void insert(tile_t c) { 
-    //printf("%d, %d\n", c.x, c.y);
-    for (int i = 0; i < tileCount; i++) {
-        tile_t *a = &tile[i];
+void insert(tile_t c, floor_t *floor) {
+    for (int i = 0; i < floor->tileCount; i++) {
+        tile_t *a = &floor->tile[i];
         if (a->x == c.x && a->y == c.y) {
-            a->on = !a->on; // flip
-            //puts(a->on ? "on" : "off");
+            a->black = !a->black; // flip
             return;
         }
     }
-    if (tileCount >= sizeof tile / sizeof tile[0]) {
+    if (floor->tileCount >= sizeof floor->tile / sizeof floor->tile[0]) {
         puts("tileCount too low");
         exit(1);
     }
-    //puts("new");
-    tile[tileCount] = c;
-    tile[tileCount++].on = true;
+    floor->tile[floor->tileCount] = c;
+    floor->tile[floor->tileCount++].black = true;
+}
+
+int countBlack(floor_t *floor) {
+    int count = 0;
+    for (int i = 0; i < floor->tileCount; i++) {
+        count += floor->tile[i].black ? 1 : 0;
+    }
+    return count;
+}
+
+int mutate(int times, floor_t *floor) {
+    const int neighbours[6][3] = { // 0=x, 1=yEven 2=yOdd   e w se sw nw ne
+        { 2, 0, 0 }, {-2, 0, 0 }, { 1, 1, 0 }, {-1, 1, 0 }, {-1, 0, -1 }, { 1, 0, -1 }
+    };
+    floor_t new;
+    for (int l = 0; l < times; l++) {
+        new.tileCount = 0;
+        // For all in the list and for all neighbours that anr not in the list
+        for (int i = 0; i < floor->tileCount; i++) {
+            tile_t *cur = &floor->tile[i];
+            int blackCount = 0;
+            for (int n = 0; n < 6; n++) {
+                int nx = cur->x + neighbours[n][0];
+                int ny = cur->y + neighbours[n][(cur->x % 2) == 0 ? 1 : 2];
+                bool neighInList = false;
+                for (int j = 0; j < floor->tileCount; j++) {
+                    tile_t *t = &floor->tile[j];
+                    if (t->x == nx && t->y == ny) {
+                        neighInList = true;
+                        blackCount += t->black ? 1 : 0;
+                    }
+                }
+                // check also neighbours not in the list
+                if (!neighInList) {
+                    bool insideReslt = false;
+                    // check if there are already in the result
+                    for (int j = 0; j < new.tileCount; j++) {
+                        if (new.tile[j].x == nx && new.tile[j].y == ny) {
+                            insideReslt = true;
+                            break;
+                        }
+                    }
+                    if (!insideReslt) {
+                        int blackCount2 = 0;
+                        tile_t non = { nx, ny, false };
+                        for (int m = 0; m < 6; m++) {
+                            int mx = non.x + neighbours[m][0];
+                            int my = non.y + neighbours[m][(nx % 2) == 0 ? 1 : 2];
+                            for (int j = 0; j < floor->tileCount; j++) {
+                                tile_t *t = &floor->tile[j];
+                                if (t->x == mx && t->y == my) {
+                                    blackCount2 += t->black ? 1 : 0;
+                                }
+                            }
+                        }
+                        if (blackCount2 == 2) {
+                            non.black = true;
+                            new.tile[new.tileCount++] = non;
+                        }
+                    }
+                }
+            }
+            if (cur->black ? !(blackCount == 0 || blackCount > 2) : blackCount == 2) {
+                tile_t c = *cur;
+                c.black = true;
+                new.tile[new.tileCount++] = c;
+            }
+        }
+        *floor = new;
+    }
+    return countBlack(floor);
 }
 
 int main(void) {
     FILE* f = fopen("input24.txt", "r");
     if (f != NULL) {
         char buf[256];
+        floor_t floor;
         while (fgets(buf, sizeof buf, f) != NULL) {
-            insert(getCoord(buf));
+            insert(getCoord(buf), &floor);
         }
         fclose(f);
-        int count = 0;
-        for (int i = 0; i < tileCount; i++) {
-            count += tile[i].on ? 1 : 0;
-        }
-        printf("Part 1: %d\n", count);
+        printf("Part 1: %d\n", countBlack(&floor));
+        printf("Part 2: %d\n", mutate(100, &floor));
     }
     return 0;
 }
