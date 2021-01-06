caa67ed40ad9d08051b11e21801e5ddd20f4f7f2
Martin Boekhoff <martin.ongsiek@gmail.com>
2021-01-06 23:48:02 +0100

    a rotated solution but looks nice. lets count seamonsters

--- aoc20.c
+++ aoc20.c
@@ -11,6 +11,8 @@ struct tile {
     int rot; // rotation of the tile
     uint16_t  sideMask[8]; // top richt bottom left and all mirroed again
     struct tile *link[4];
+    struct tile *right;
+    struct tile *down;
 };
 
 typedef struct tile tile_t;
@@ -23,6 +25,7 @@ const char seaMonster[] = "                  # "
                           "#    ##    ##    ###"
                           " #  #  #  #  #  #   ";
 int seaMonsterOffset[15][2];
+void printTileRot(tile_t *t);
 void calcSeamonsterOffsets(void) {
     char *s = (char*) seaMonster;
     int index = 0;
@@ -97,7 +100,19 @@ char get2(tile_t *t, int x, int y) {
     return getAdv((char*)t->data, t->rot, x, y, 10);
 }
 
-bool fit(tile_t *left, tile_t *right) {
+uint16_t getSideMask(tile_t *t, bool right) {
+    uint16_t mask = 0;
+    for (int j = 0; j < 10; j++) {
+        if (right) {
+            mask |= get2(t, 9, j) == '#' ? 1 << j : 0; 
+        } else {
+            mask |= get2(t, j, 9) == '#' ? 1 << j : 0; 
+        } 
+    }
+    return mask;
+}
+
+bool fitRight(tile_t *left, tile_t *right) {
     for (int i = 0; i < 10; i++) {
         if (get2(left, 9, i) != get2(right, 0, i)) {
             return false;
@@ -106,25 +121,75 @@ bool fit(tile_t *left, tile_t *right) {
     return true;
 }
 
-void printTileRot(tile_t *t, int rot) {
-    printf("%d rot %d\n", t->id, rot);
+bool fitDown(tile_t *up, tile_t *down) {
+    for (int i = 0; i < 10; i++) {
+        if (get2(up, i, 9) != get2(down, i, 0)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 
+
+
+bool setRotation(tile_t *pre, tile_t *cur, bool right) {
+    int linkCountCur = 0, linkCountPrev = 0;
+    for (int j = 0; j < 4; j++) {
+        linkCountCur  += cur->link[j] != 0;
+        if (pre != 0) {
+            linkCountPrev += pre->link[j] != 0;
+        } 
+    }
+    bool end = linkCountPrev > linkCountCur;
+    bool checkRight = (right && !end) || !right;
+    bool checkDown  = (!right && !end);
+    for (int i = 0; i < 8; i++) {
+        cur->rot = i;
+        if (pre == 0 || (right ? fitRight(pre, cur) : fitDown(pre, cur))) {
+            printTileRot(cur);
+            int  countMatch = 0;
+            for (int j = 0; j < 4; j++) {
+                if (checkRight && cur->link[j] != 0 && pre != cur->link[j]) {
+                    if (setRotation(cur, cur->link[j], true)) {
+                        printf("%d right %d(rot=%d)\n", cur->id, cur->link[j]->id, cur->link[j]->rot);
+                        cur->right = cur->link[j];
+                        countMatch++;
+                    }
+                }
+            }
+            for (int j = 0; j < 4; j++) {
+                if (checkDown && cur->link[j] != 0 && pre != cur->link[j]) {
+                    if (setRotation(cur, cur->link[j], false)) {
+                        printf("%d down %d(rot=%d)\n", cur->id, cur->link[j]->id, cur->link[j]->rot);
+                        cur->down = cur->link[j];
+                        countMatch++;
+                    }
+                }
+            }
+            if (countMatch == (checkRight + checkDown)) {
+                printf("id %d match rot=%d\n", cur->id, cur->rot);
+                return true;
+            } else {
+                cur->right = cur->down = 0;
+            }
+        }
+    }
+    
+    return false;
+}
+
+void printTileRot(tile_t *t) {
+    printf("%d rot %d\n", t->id, t->rot);
     for (int y = 0; y < 10; y++) {
         for (int x = 0; x < 10; x++) {
-            printf("%c", get(t->data, rot, x, y));
+            printf("%c", get(t->data, t->rot, x, y));
         }
         puts("");
     }
     puts("");
 }
-int getRot(int sideMask) {
-    return  sideMask == 3 ? 0 :  
-            sideMask == 2 ? 1 :
-            sideMask == 1 ? 2 :
-            sideMask == 4 ? 3 :
-            sideMask == 5 ? 4 :
-            sideMask == 7 ? 5 :
-            sideMask == 0 ? 6 : 7;
-}
+
 /*  rotation mirrorin combinations
 sides   ! means reversed
     1     x123
@@ -140,71 +205,20 @@ sides   ! means reversed
  4 3 2 1  fl      x  y  
 !2!1!4!3  mir fl !x !y   
  */
-int getSidemaskLeft(int rot) {
-    return rot == 0 ? 3 :
-           rot == 1 ? 2 :
-           rot == 2 ? 5 :
-           rot == 3 ? 4 :
-           rot == 4 ? 1 :
-           rot == 5 ? 7 :
-           rot == 6 ? 0 : 6;
+
+char *copyImageRow(char *result, tile_t *t, int row) {
+    // for (int i = 1; i < 9; i++) {
+    //     *result++ = get(t->data, t->rot, i, row);
+    // }
+    // if (t->link[next] == 0) {
+    //     return result;
+    // } else {
+    //     return copyImageRow(result, t->link[next], row);
+    // }
+    return result;
 }
 
-tile_t *getDown(tile_t *t) {
-    int i = getSidemaskLeft(t->rot);
-    int down = (i + 1) % 4;
-    tile_t *ret = t->link[down];
-    int downMask = t->sideMask[i > 3 ? down : down + 4];
-    for (i = 0; i < 8; i++) {
-        if (downMask == ret->sideMask[i]) {
-            int n = (i + 1) % 4;
-            ret->rot = getRot(i > 3 ? n + 4 : n);
-            break;
-        }
-    }
-    return ret;
-}
 
-void oriantateFirst(tile_t *t) {
-    for (int rot = 0; rot < 8; rot++) {
-        int i = getSidemaskLeft(rot);
-        int up = (i + 1) % 4;
-        int left = i % 4;
-        if (t->link[up] == 0 && t->link[left] == 0) {
-            t->rot = i;
-        }
-    }
-}
-
-char *copyImageRow(char *result, tile_t *t, int row, int rightMask) {
-    // right 0 1 2 3 4 5 6 7
-    //       1 2 3 4!1!2!3!4
-    // rot   3 4 1 0   2   5
-    int next = 0, nextMask;
-    if (rightMask == 0) {
-        int i = getSidemaskLeft(t->rot);
-        next = (i + 2) % 4;
-        nextMask = i > 3 ? next : next + 4;
-    } else {
-        for (int i = 0; i < 8; i++) {
-            if (rightMask == t->sideMask[i]) {
-                t->rot = getRot(i);
-                next = (i + 2) % 4;
-                nextMask = i > 3 ? next : next + 4;
-                break;
-            }
-        }
-    }
-    printTileRot(t, t->rot);
-    for (int i = 1; i < 9; i++) {
-        *result++ = get(t->data, t->rot, i, row);
-    }
-    if (t->link[next] == 0) {
-        return result;
-    } else {
-        return copyImageRow(result, t->link[next], row, t->sideMask[nextMask]);
-    }
-}
 
 void puzzzle() {
     // link puzzel tiles
@@ -224,44 +238,23 @@ void puzzzle() {
             }
         }
     }
-    char result[8*8*tileCount + 1];
-    memset(result, 0, 8*8*tileCount + 1);
-    // start with the first corner
-    char *r = result;
-    oriantateFirst(&tile[firstCorner]);
-    for (int i = 1; i < 2/*9*/; i++) {
-        r = copyImageRow(r, &tile[firstCorner], i, 0);
-    }
-    int lineLen = strlen(result);
-    tile_t *t = getDown(&tile[firstCorner]);
-    r = copyImageRow(r, t, 1, 0);
-    tile_t *t2 = getDown(t);
-    r = copyImageRow(r, t2, 1, 0);
+    setRotation(0, &tile[firstCorner], false);
+    // char result[8*8*tileCount + 1];
+    // memset(result, 0, 8*8*tileCount + 1);
+    // // start with the first corner
+    // char *r = result;
+    // oriantateFirst(&tile[firstCorner]);
+    // for (int i = 1; i < 2/*9*/; i++) {
+    //     r = copyImageRow(r, &tile[firstCorner], i, 0);
+    // }
+    // int lineLen = strlen(result);
+    // tile_t *t = getDown(&tile[firstCorner]);
+    // r = copyImageRow(r, t, 1, 0);
+    // tile_t *t2 = getDown(t);
+    // r = copyImageRow(r, t2, 1, 0);
 }
 
 #define ASSERT_EQ(a, b, r, c) {if (a != b) printf("%s:%d error %d %d "#a" (%d) != "#b" (%d)\n", __FILE__, __LINE__, r, c, a, b); }
-void rotTest(void) {
-    tile_t *t = &tile[1];
-    for (int rot = 0; rot < 8; rot++) {
-        int curMask[4] = {0};
-        for (int j = 0; j < 10; j++) {
-            int k = 9 - j;
-            uint16_t bitMask = (1 << j);
-            curMask[0] |= (get(t->data, rot, j, 0) == '#' ? bitMask : 0);
-            curMask[1] |= (get(t->data, rot, 9, j) == '#' ? bitMask : 0);
-            curMask[2] |= (get(t->data, rot, k, 9) == '#' ? bitMask : 0);
-            curMask[3] |= (get(t->data, rot, 0, k) == '#' ? bitMask : 0);
-        }
-        int left = getSidemaskLeft(rot);
-        for (int i = 0; i < 4; i++) {
-            int m = left + i;
-            int n = m % 4;
-            int mask = t->sideMask[m > 0 ? n : n + 4];
-            ASSERT_EQ(curMask[i], mask, rot, i);
-        }
-    }
-}
-
 
 int main(void) {
     FILE* f = fopen("test20.txt", "r");
@@ -298,7 +291,9 @@ int main(void) {
         printf("part 1: corner product %lld\n", calcSides());
         calcSeamonsterOffsets();
         puzzzle();
-        rotTest();
+        // for (int i = 0; i < tileCount; i++) {
+        //     printTileRot(&tile[i]);
+        // }
     }
     return 0;
 }
